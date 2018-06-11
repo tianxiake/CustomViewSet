c14932b4b9480c98545697ad20989944ff25ec33
liuyongjie <liuyongjie@droi.com>
2018-06-11 17:16:58 +0800

    自定义的viewpager可以滑动

--- app/src/main/java/com/snail/customviewset/MainActivity.java
+++ app/src/main/java/com/snail/customviewset/MainActivity.java
@@ -3,16 +3,34 @@ package com.snail.customviewset;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.snail.customviewset.view.CustomFlowLayout;
+import com.snail.customviewset.view.MyViewPager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int[] drawables = new int[]{
+            R.drawable.drawable_one,
+            R.drawable.drawable_two,
+            R.drawable.drawable_three,
+            R.drawable.drawable_four
+    };
+
+    private MyViewPager myViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cutom_simple_relative_layout);
+        setContentView(R.layout.activity_custom_my_view_pager);
 
+        myViewPager = findViewById(R.id.mvp_layout);
+
+        for (int i = 0; i < drawables.length; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setImageResource(drawables[i]);
+            myViewPager.addView(imageView);
+        }
     }
 }
--- app/src/main/java/com/snail/customviewset/view/MyViewPager.java
+++ app/src/main/java/com/snail/customviewset/view/MyViewPager.java
@@ -2,9 +2,16 @@ package com.snail.customviewset.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.ViewGroup;
 
 public class MyViewPager extends ViewGroup {
+
+    private static String LYJ_TAG = "LYJ_MyViewPager";
+    private GestureDetector gestureDetector;
+
     public MyViewPager(Context context) {
         this(context, null);
     }
@@ -15,6 +22,18 @@ public class MyViewPager extends ViewGroup {
 
     public MyViewPager(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        gestureDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                Log.d(LYJ_TAG, "distanceX=" + distanceX + ",distanceY=" + distanceY);
+                scrollBy((int) distanceX, 0);
+                return true;
+            }
+        });
     }
 
     @Override
@@ -24,5 +43,36 @@ public class MyViewPager extends ViewGroup {
         for (int i = 0; i < childCount; i++) {
             getChildAt(i).layout(i * getWidth(), 0, (i + 1) * getWidth(), getHeight());
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(LYJ_TAG, "event=" + event.getAction());
+        gestureDetector.onTouchEvent(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                int scrollX = getScrollX();
+                int pageIndex = scrollX / getWidth();
+                int offset = scrollX % getWidth();
+                Log.d(LYJ_TAG, "scrollX=" + scrollX + ",pageIndex=" + pageIndex + ",offset=" + offset);
+                if (offset > getWidth() / 2) {
+                    //下一页
+                    pageIndex++;
+                }
+                if (pageIndex > getChildCount() - 1) {
+                    pageIndex = getChildCount() - 1;
+                }
+                getCurrentPage(pageIndex);
+                break;
+        }
+        return true;
+    }
+
+    public void getCurrentPage(int pageIndex) {
+        scrollTo(pageIndex * getWidth(), 0);
     }
 }
